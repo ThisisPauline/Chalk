6856c065d3ebca70ccfb7d7c4a18ead7fc406684
Pauline Kraus <kraus.pauline1@gmail.com>
2022-09-29 15:35:03 +0200

    chalk name version 1

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.red.bgBlue("Osvaldo"));
+console.log(chalk.blue.bgRed("Russell"));
+console.log(chalk.green.bgGrey("Anna"));
+console.log(chalk.yellow.bgWhite("Pierre"));
